7df5af7e859cebc5538ace2b37bfb08b322abafc
kouzie <kgy1996@naver.com>
2023-01-12 08:54:09 +0900

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,7 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
 
       <v-btn
@@ -36,7 +17,7 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6">
@@ -82,6 +63,7 @@ export default {
   name: 'App',
   data() {
     return {
+      drawer: true,
       items: [
         {title: 'Dashboard', icon: 'mdi-view-dashboard', to: "/"},
         {title: 'GridSystem', icon: 'mdi-view-dashboard', to: "/grid-system"},
