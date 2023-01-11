5a059e07db84334cff933e9be79623d1086c3938
kouzie <kgy1996@naver.com>
2023-01-12 06:57:35 +0900

    add vuetify

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### ES-Lint-Vuetify install
+
+https://www.npmjs.com/package/eslint-plugin-vuetify/v/1.0.0-beta.5?activeTab=readme
+npm install eslint-plugin-vuetify --save-dev
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Dashboard from "@/views/Dashboard.vue";
+import GridSystem from "@/views/GridSystem.vue";
+
+Vue.use(VueRouter)
+
+const routes = [{
+  path:"/",
+  name: "Dashboard",
+  component: Dashboard
+},{
+  path:"/grid-system",
+  name: "GridSystem",
+  component: GridSystem
+}]
+
+const router = new VueRouter({
+  mode:"history", 
+  routes
+})
+
+export default router;
